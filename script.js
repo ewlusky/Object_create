e6c236037f62dc6ca83b7616f171a79f2d97c303
Evan <ewlusky@gmail.com>
2018-06-01 09:44:25 -0500

    Added job objects

---
+++ script.js
@@ -0,0 +1,42 @@
+const Navy = Object.create({}, {
+    business: {
+        value: "United States Navy"
+    },
+    role: {
+        value: "Cryptologic Technician: Interpretive"
+    }
+    employmentStart: {
+        value: "30MAY2009"
+    },
+    employmentEnd: {
+        value: "31MAY2015"
+    },
+    briefsAttended: {
+        value: 3854
+    },
+    briefsNecessary: {
+        value: 8
+    }
+})
+
+const Vet = Object.create({}, {
+    business: {
+        value: "Veterinary Hospital"
+    },
+    role: {
+        value: "Veterinary Technician"
+    },
+    employmentStart: {
+        value: "06OCT2008"
+    },
+    employmentEnd: {
+        value: "15DEC2008"
+    },
+    scratches: {
+        value: 866
+    },
+    bites: {
+        value: 6
+    }
+    
+})
